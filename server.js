3ce930276855b1d07ac704b7e7e084549e8e71b9
Manav Verma <manav@Manavs-Macbook-Air.local>
2021-08-15 17:07:47 +0530

    adds Compplete code for dtraftstorm-server: code improv, architecture improc, bug fixes, error handling, - Ready to deploy :}

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const utils = require('./utils');
 const express = require('express')
 const socket = require('socket.io')
 const http = require('http')
@@ -22,56 +23,88 @@ const io = socket(server
   }
 );
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+const {
+  addUser, removeUser, getUser, showUsers, getRooms,
+} = require('./users');
+
+app.get('/rooms', (req, res) => {
+  const rooms = getRooms();
+  console.log("rooms : ", rooms);
+  res.send({ rooms: rooms });
+})
 
 io.on('connection', (socket) => {
 
   // console.log("User Connected : ", socket.id);
-  socket.on("joinRoom", function ({ userName, roomName }, callback) {
+  socket.on(utils.MESSAGE_TYPES.JOIN_ROOM, function ({ userName, roomName }, callback) {
 
     const { error, newUser } = addUser({ socketId: socket.id, userName, roomName });
+    console.log("error : ", error);
+    console.log("newUser : ", newUser);
+    // showUsers();
 
     if (error) {
-      // Error Case
-      return callback(error);
+      socket.emit(
+        utils.MESSAGE_TYPES.ERROR, {
+        text: error,
+        type: utils.MESSAGE_CLASSES.RED_FLAG,
+      }
+      );
+      return;
     }
     socket.join(newUser.roomName);
-    socket.emit("selfWelcome", { text: `Welcome to the Room : ${newUser.roomName}` });
-    socket.broadcast.to(newUser.roomName).emit("userNotification", { text: `${newUser.userName} has joined the Room.` });
-    // callback();
+    socket.emit(
+      utils.MESSAGE_TYPES.NOTIFICATION, {
+      text: `You have joined the Room : ${newUser.roomName}`,
+      type: utils.MESSAGE_CLASSES.GREEN_FLAG,
+    }
+    );
+    socket.broadcast.to(newUser.roomName).emit(
+      utils.MESSAGE_TYPES.NOTIFICATION, {
+      text: `${newUser.userName} has joined the Room.`,
+      type: utils.MESSAGE_CLASSES.GREEN_FLAG,
+    }
+    );
   })
 
-  socket.on("sendMsg", (message, callback) => {
+  // Message Functionality #TBD
+  // socket.on("sendMsg", (message, callback) => {
+  //   const currUser = getUser(socket.id);
+  //   io.to(currUser.roomName).emit('msg', { userName: currUser.userName, text: message })
+  //   callback();
+  // })
 
-    const currUser = getUser(socket.id);
-    io.to(currUser.roomName).emit('msg', { userName: currUser.userName, text: message })
-    callback();
-  })
-
-  socket.on('c_mouse_down', (payload) => {
+  socket.on(utils.MOUSE_EVENTS.CLIENT_DOWN, (payload) => {
 
     console.log("c_mouse_down payload RCVD : ", payload)
     const currUser = getUser(socket.id);
-    io.to(currUser.roomName).emit('s_mouse_down', payload);
+    io.to(currUser.roomName).emit(utils.MOUSE_EVENTS.SERVER_DOWN, payload);
   })
 
-  socket.on('c_mouse_move', (payload) => {
+  socket.on(utils.MOUSE_EVENTS.CLIENT_MOVE, (payload) => {
 
     console.log("c_mouse_move payload RCVD : ", payload)
     const currUser = getUser(socket.id);
-    io.to(currUser.roomName).emit('s_mouse_move', payload);
+    io.to(currUser.roomName).emit(utils.MOUSE_EVENTS.SERVER_MOVE, payload);
   })
 
   socket.on('disconnect', (reason) => {
 
     console.log(reason, '=> Disconnected : ', socket.id);
     const leavingUser = removeUser(socket.id);
+    // showUsers();
+    console.log("Leaving User : ", leavingUser);
     if (leavingUser) {
-      socket.broadcast.to(leavingUser.roomName).emit("userNotification", { text: `${leavingUser.userName} has Left the Room.` });
+      socket.broadcast.to(leavingUser.roomName).emit(
+        utils.MESSAGE_TYPES.NOTIFICATION, {
+        text: `${leavingUser.userName} has Left the Room.`,
+        type: utils.MESSAGE_CLASSES.RED_FLAG,
+      }
+      );
     }
   });
 })
 
 server.listen(PORT, () => {
-  console.log("Listening on : http://127.0.0.1:" + PORT);
+  console.log("Listening on : http://localhost:" + PORT);
 })
--- users.js
+++ users.js
@@ -1,9 +1,9 @@
 let users = [];
 
-function addUser({ socketId, userName, roomName }) {
-  // Tranform userName and roomName into one word.
-  // Manav Verma => manavverma
+let roomParticipantMap = {};
 
+function addUser({ socketId, userName, roomName }) {
+  // Tranform userName and roomName into one word., e.g. :Manav Verma => manavverma
 
   userName = userName.trim().toLowerCase();
   roomName = roomName.trim().toLowerCase();
@@ -13,23 +13,58 @@ function addUser({ socketId, userName, roomName }) {
 
   // if (!userName || !roomName) return { error: 'Username and roomName are required.' };
   if (existingUser) {
+    // Emit Error Message when Username is occupied
     return { error: 'Username is taken.' };
   }
   const newUser = { socketId, userName, roomName };
   console.log("newUser", newUser);
   users.push(newUser);
-  // console.log("Users :", users)
+  addRoom(roomName);
   return { newUser };
 }
 
 const removeUser = (socketId) => {
   const index = users.findIndex((user) => user.socketId === socketId);
 
-  if (index !== -1) return users.splice(index, 1)[0];
+  if (index !== -1) {
+    const { roomName } = users[index];
+    removeRoom((roomName))
+    return users.splice(index, 1)[0];
+  }
 }
 
 const getUser = (socketId) => users.find((user) => user.socketId === socketId);
 
 const getUsersInRoom = (roomName) => users.filter((user) => user.roomName === roomName);
 
-module.exports = { addUser, removeUser, getUser, getUsersInRoom };
+const showUsers = () => {
+  console.log("All Users : ")
+  users.map((user) => {
+    console.log(user);
+    return user;
+  });
+}
+
+const getRooms = () => Object.keys(roomParticipantMap);
+
+const addRoom = (roomName) => {
+  if (roomParticipantMap[roomName]) {
+    roomParticipantMap[roomName] += 1;
+  } else {
+    roomParticipantMap[roomName] = 1;
+  }
+};
+
+const removeRoom = (roomName) => {
+  if (roomParticipantMap[roomName]) {
+    if (roomParticipantMap[roomName] > 1) {
+      roomParticipantMap[roomName] -= 1;
+    } else {
+      delete roomParticipantMap[roomName];
+    }
+  }
+};
+
+module.exports = {
+  addUser, removeUser, getUser, getUsersInRoom, showUsers, getRooms,
+};
